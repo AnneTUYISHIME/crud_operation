8474d4d56150c479d47a8ddb612bc664b3a0816f
Tuyishime Anne <atuyishime28@gmail.com>
2025-05-13 00:22:18 +0300

    student crud by API backend

--- student_api/server.js
+++ student_api/server.js
@@ -1,24 +1,31 @@
 // server.js
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const studentRoutes = require('./routes/studentRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const studentRoutes = require("./routes/studentRoutes");
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/students', studentRoutes);
+app.use("/students", studentRoutes);
 
 // MongoDB connection
-mongoose.connect('mongodb://127.0.0.1:27017/studentDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('✅ MongoDB Connected');
-  app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
-}).catch((err) => {
-  console.error('❌ MongoDB connection failed:', err.message);
-});
-
+mongoose
+  .connect(
+    "mongodb+srv://Anne:Amanibaba@cluster0.wzwxkny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+    app.listen(PORT, () =>
+      console.log(`🚀 Server running at http://localhost:${PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection failed:", err.message);
+  });
